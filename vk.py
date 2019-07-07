2dbf2dda74e7464e12069ab3c670dde1be23f3aa
seilkhanovkkk <seil.kuka@gmail.com>
2019-07-07 07:45:57 +0600

    vk updated

--- vk.py
+++ vk.py
@@ -13,7 +13,7 @@ id = "7046828"
 
 URL = "https://api.vk.com/method/users.get"
 
-PARAMS = {'user_id': 184058947,'v': 5.95, 'access_token': token, "fields": "city,bdate,sex,verified,contacts,site,education,last_seen,followers_count,counters,relation,personal,activities,interests,music,movies,tv,books,games,connections"}
+PARAMS = {'user_id': 184058947,'v': 5.95, 'access_token': token, "fields": "city,bdate,sex,last_seen,counters,relation"}
 
 r = requests.get(url = URL, params = PARAMS)
 data = r.json()
@@ -25,4 +25,18 @@ PARAMS1 = {"user_id": 184058947, "v": 5.95, "access_token": token, "fields": "co
 
 r1 = requests.get(url = URL1, params = PARAMS1)
 data1 = r1.json()
-print(data1['response']['count'])
+json_string = json.dumps(data)
+json_string = json_string.replace("[", "")
+json_string = json_string.replace("]", "")
+data = json.loads(json_string)
+user = {
+    "name": data['response']["first_name"],
+    "surname": data['response']["last_name"],
+    "sex": data['response']["sex"],
+    "city": data['response']['city']['title'],
+    "last_seen": data['response']["last_seen"]['time'],
+    "pages": data['response']["counters"]['pages'],
+    "friends": data1['response']["count"],
+    "bdate": data['response']['bdate']
+}
+print(user)
